6ea7e7bef6ff1f0c716c2983c05dce37a8a01b08
Ibrahim <ibrahimsabat@yahoo.com>
2018-10-24 14:28:13 +0100

    Answer Task for H-array-literals

--- week-2/H-array-literals/exercise.js
+++ week-2/H-array-literals/exercise.js
@@ -4,8 +4,8 @@
   Declare some variables assigned to arrays of values
 */
 
-var numbers = []; // add numbers from 1 to 10 into this array
-var mentors; // Create an array with the names of 4 mentors
+var numbers = [1,4,2,6,8,9,6,4,3]; // add numbers from 1 to 10 into this array
+var mentors = ["Ibrahim", "Ali", "Salm", "Bader"]; // Create an array with the names of 4 mentors
 
 /* 
   DO NOT EDIT BELOW THIS LINE
